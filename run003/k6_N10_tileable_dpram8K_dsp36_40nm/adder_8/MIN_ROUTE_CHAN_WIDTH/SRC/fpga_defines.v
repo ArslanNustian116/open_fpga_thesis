a0291be1cb1fdc6e00e92fb84ee69c3174b1ab65
Arslan Majid <arslan@iqraisb.edu.pk>
2025-09-05 11:51:38 +0500

    Trying it mate

---
+++ run003/k6_N10_tileable_dpram8K_dsp36_40nm/adder_8/MIN_ROUTE_CHAN_WIDTH/SRC/fpga_defines.v
@@ -0,0 +1,12 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Fri Sep  5 11:31:38 2025
+//-------------------------------------------
+//----- Time scale -----
+`timescale 1ns / 1ps
+
+`define ENABLE_TIMING 1
+
